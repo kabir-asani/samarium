2f3254399b188ee7689224be9a4777167434bea2
Kabir Asani <iamkabirasani@gmail.com>
2025-03-18 12:09:16 +0530

    Implement CRUD operations for countries using Prisma

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Samarium
+
+1. `GET` `/countries`
+2. `POST` `/countries`
+
+```json
+{
+  "name": "United States Of America",
+  "countryCode": "USA"
+}
+```
+
+3. `PATCH` `/countries/:countryCode`
+
+```json
+{
+  "name": "United States Of America"
+}
+```
+
+4. `DELETE` `/countries/:countryCode`
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,86 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { prismaClient } from "./prisma";
 
-const app = new Hono();
+const hono = new Hono();
 
-app.get("/", (context) => {
-  return context.json({ message: "Hello, World!" }, 200);
+hono.get("/countries", async (context) => {
+  const countries = await prismaClient.country.findMany();
+
+  return context.json(
+    {
+      countries,
+    },
+    200
+  );
 });
 
-serve(app, (info) => {
+hono.post("/countries", async (context) => {
+  const { name, countryCode } = await context.req.json();
+
+  const country = await prismaClient.country.create({
+    data: {
+      name,
+      countryCode,
+    },
+  });
+
+  return context.json(
+    {
+      country,
+    },
+    201
+  );
+});
+
+hono.patch("/countries/:countryCode", async (context) => {
+  const { countryCode } = context.req.param();
+  const { name } = await context.req.json();
+
+  const country = await prismaClient.country.update({
+    where: {
+      countryCode,
+    },
+    data: {
+      name,
+    },
+  });
+
+  return context.json(
+    {
+      country,
+    },
+    200
+  );
+});
+
+hono.delete("/countries/:countryCode", async (context) => {
+  const { countryCode } = context.req.param();
+
+  const existingCountry = await prismaClient.country.findUnique({
+    where: {
+      countryCode,
+    },
+  });
+
+  if (existingCountry) {
+    await prismaClient.country.delete({
+      where: {
+        countryCode,
+      },
+    });
+
+    return context.json(
+      {
+        country: existingCountry,
+      },
+      200
+    );
+  } else {
+    return context.notFound();
+  }
+});
+
+serve(hono, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`);
 });
